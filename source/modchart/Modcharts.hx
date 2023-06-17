69268d885349d3a4433e2108112a48329151cece
Kn1ghtNight <josephj108xb@gmail.com>
2023-06-17 17:03:45 -0400

    i can finally krill my shelf

--- source/modchart/Modcharts.hx
+++ source/modchart/Modcharts.hx
@@ -60,114 +60,40 @@ class Modcharts {
                 modManager.queueEaseP(1152, 1156, 'tipZ', 145);
                 modManager.queueEaseP(1708, 1712, 'tipZ', 0);
             case 'endeavors':
-
-                var kicks = [];
-                numericForInterval(144, 672, 8, function(i){
-                    kicks.push(i);
-                });
-
-                for (i in 0...kicks.length){
-                    var step = kicks[i];
-                    modManager.queueSetP(step, 'tipsy', 125);
-                    modManager.queueSetP(step, 'tipsyOffset', 25);
-                    modManager.queueSetP(step, "squish", 35);
-                    modManager.queueSetP(step, "beat", 75);
-                    modManager.queueSet(step, 'transformX', -75);
-                    modManager.queueSetP(step, 'mini', -25);
-                    modManager.queueEaseP(step, step + 4, "squish", 0, "circOut");
-                    modManager.queueEaseP(step, step + 4, "beat", 0, "circOut");
-                    modManager.queueEase(step, step + 4, 'transformX', 0, 'circOut');
-                    modManager.queueEaseP(step, step + 4, 'tipsy', 0, 'circOut');
-                    modManager.queueEaseP(step, step + 4, 'tipsyOffset', 0, 'cubeOut');
-                    modManager.queueEaseP(step, step + 4, "mini", 0, "quadOut");
-                }
-
-                modManager.queueFunc(144, 672, function(event:CallbackEvent, cDS:Float){
-                    var s = cDS - 144;
-                    var beat = s / 4;
-                    modManager.setValue("transformY-a", -60 * Math.abs(Math.sin(Math.PI * beat)));
-                    modManager.setValue("transformX-a", 30 * Math.cos(Math.PI * beat));
-                });
-
-                modManager.queueEaseP(672, 676, "transformY-a", 0, 'quadOut');
-                modManager.queueEase(672, 676, "transformX-a", 0, 'quadOut');
-
-                var kicks2 = [];
-                numericForInterval(736, 1778, 8, function(i){
-                    kicks2.push(i);
-                });
-
-                for (i in 0...kicks2.length){
-                    var step = kicks2[i];
-                    modManager.queueSetP(step, 'tipsy', 125);
-                    modManager.queueSetP(step, 'tipsyOffset', 25);
-                    modManager.queueSetP(step, "squish", 35);
-                    modManager.queueSetP(step, "beat", 75);
-                    modManager.queueSet(step, 'transformX', -75);
-                    modManager.queueSetP(step, 'mini', -25);
-                    modManager.queueEaseP(step, step + 4, "squish", 0, "circOut");
-                    modManager.queueEaseP(step, step + 4, "beat", 0, "circOut");
-                    modManager.queueEase(step, step + 4, 'transformX', 0, 'circOut');
-                    modManager.queueEaseP(step, step + 4, 'tipsy', 0, 'circOut');
-                    modManager.queueEaseP(step, step + 4, 'tipsyOffset', 0, 'cubeOut');
-                    modManager.queueEaseP(step, step + 4, "mini", 0, "quadOut");
-                }
-
-                modManager.queueFunc(736, 1778, function(event:CallbackEvent, cDS:Float){
-                    var s = cDS - 736;
-                    var beat = s / 4;
-                    modManager.setValue("transformY-a", -60 * Math.abs(Math.sin(Math.PI * beat)));
-                    modManager.setValue("transformX-a", 30 * Math.cos(Math.PI * beat));
-                });
-
-                //speeen -- take long note step, subtract 4 MAKE SURE STEP IS EVEN I BEG OF YOU :SOB:
-                modManager.queueEase(396, 400, "centerrotateX", FlxAngle.asRadians(85), "quadIn");
-                modManager.queueEase(400, 410, "centerrotateX", FlxAngle.asRadians(-360)*3, "elasticOut");
-                modManager.queueSet(410, "centerrotateX", 0);
-
-                modManager.queueEase(986, 990, "centerrotateY", FlxAngle.asRadians(85), "quadIn");
-                modManager.queueEase(990, 1000, "centerrotateY", FlxAngle.asRadians(-360)*3, "elasticOut");
-                modManager.queueSet(1000, "centerrotateY", 0);
-
-                modManager.queueEase(1242, 1246, "centerrotateY", FlxAngle.asRadians(85), "quadIn");
-                modManager.queueEase(1246, 1256, "centerrotateY", FlxAngle.asRadians(-360)*3, "elasticOut");
-                modManager.queueSet(1256, "centerrotateY", 0);
-
-                modManager.queueEaseP(1778, 1779, "transformY-a", 0, 'quadOut');
-                modManager.queueEase(1778, 1779, "transformX-a", 0, 'quadOut');
-
                 var kicks3 = [];
-                numericForInterval(1780, 2031, 8, function(i){
-                    kicks3.push(i);
-                });
+				numericForInterval(144, 672, 4, function(i)
+				{
+					kicks3.push(i);
+				});
 
-                for (i in 0...kicks3.length){
-                    var step = kicks3[i];
-                    modManager.queueSetP(step, 'tipsy', 125);
-                    modManager.queueSetP(step, 'tipsyOffset', 25);
-                    modManager.queueSetP(step, "squish", 35);
-                    modManager.queueSet(step, 'transformX', -75);
-                    modManager.queueSetP(step, "beat", 75);
-                    modManager.queueSetP(step, 'mini', -25);
-                    modManager.queueEaseP(step, step + 4, "squish", 0, "circOut");
-                    modManager.queueEaseP(step, step + 4, "beat", 0, "circOut");
-                    modManager.queueEase(step, step + 4, 'transformX', 0, 'circOut');
-                    modManager.queueEaseP(step, step + 4, 'tipsy', 0, 'circOut');
-                    modManager.queueEaseP(step, step + 4, 'tipsyOffset', 0, 'cubeOut');
-                    modManager.queueEaseP(step, step + 4, "mini", 0, "quadOut");
-                }
-
-                modManager.queueFunc(1780, 2031, function(event:CallbackEvent, cDS:Float){
-                    var s = cDS - 1780;
-                    var beat = s / 4;
-                    modManager.setValue("transformY-a", -60 * Math.abs(Math.sin(Math.PI * beat)));
-                    modManager.setValue("transformX-a", 30 * Math.cos(Math.PI * beat));
-                });
-
-                modManager.queueEaseP(1904, 1908, "infinite", 100, "quadOut", 0);
-                modManager.queueEaseP(1904, 1908, "alpha", 100, "quadOut", 1);
-                modManager.queueEaseP(2032, 2034, "infinite", 0, "quadOut", 0);
-                modManager.queueEaseP(2046, 2052, "alpha", 100, "quadOut", 0);
+                var m = 1;
+				for (i in 0...kicks3.length)
+				{
+                    m = m * -1;
+					var step = kicks3[i];
+                    modManager.queueSet(step, 'transform0Y', 50 * m);
+					modManager.queueSet(step, 'transform1Y', 50 * m);
+					modManager.queueSet(step, 'transform2Y', -50 * m);
+					modManager.queueSet(step, 'transform3Y', -50 * m);
+                    modManager.queueSet(step, 'transform0Z', 0.5 * m);
+					modManager.queueSet(step, 'transform1Z', 0.5 * m);
+					modManager.queueSet(step, 'transform2Z', -0.5 * m);
+					modManager.queueSet(step, 'transform3Z', -0.5 * m);
+					modManager.queueSetP(step, "reverse", 25);
+					modManager.queueSetP(step, "squish", 55);
+					modManager.queueSetP(step, 'opponentSwap', 25);
+					modManager.queueEaseP(step, step + 4, "squish", 0, "circOut");
+					modManager.queueEaseP(step, step + 4, "reverse", 0, "circOut");
+					modManager.queueEaseP(step, step + 4, 'opponentSwap', 0, 'circOut');
+                    modManager.queueEase(step, step + 6, 'transform0Y', 0, 'circOut');
+					modManager.queueEase(step, step + 6, 'transform1Y', 0, 'circOut');
+					modManager.queueEase(step, step + 6, 'transform2Y', 0, 'circOut');
+					modManager.queueEase(step, step + 6, 'transform3Y', 0, 'circOut');
+                    modManager.queueEase(step, step + 6, 'transform0Z', 0, 'circOut');
+					modManager.queueEase(step, step + 6, 'transform1Z', 0, 'circOut');
+					modManager.queueEase(step, step + 6, 'transform2Z', 0, 'circOut');
+					modManager.queueEase(step, step + 6, 'transform3Z', 0, 'circOut');
+				}
             }         
         }
     }
